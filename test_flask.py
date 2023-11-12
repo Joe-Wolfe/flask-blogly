fceb9e71f9f95d00d6ef44a9829835e9a11da5b6
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-11-12 17:16:47 -0500

    Added testing for Posts. and finished exercise 2

--- test_flask.py
+++ test_flask.py
@@ -1,6 +1,6 @@
 import unittest
 from app import app, db
-from models import User
+from models import User, Post
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
@@ -88,3 +88,53 @@ class BloglyTestCase(unittest.TestCase):
 
             self.assertEqual(resp.status_code, 200)
             self.assertNotIn("Test User", html)
+
+    def test_show_create_post_form(self):
+        """Test if show_create_post_form shows the form to create a new post"""
+        with app.test_client() as client:
+            response = client.get(f"/create_post/{self.user_id}")
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("New Post", response.get_data(as_text=True))
+
+    def test_create_post(self):
+        """Test if create_post creates a new post"""
+        with app.test_client() as client:
+            response = client.post(f"/create_post/{self.user_id}", data={
+                "title": "Test Title", "content": "Test Content"}, follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(Post.query.count(), 1)
+            self.assertEqual(Post.query.first().title, "Test Title")
+
+    def test_show_post(self):
+        """Test if show_post shows the selected post"""
+        with app.test_client() as client:
+            post = Post(title="Test Title", content="Test Content",
+                        user_id=self.user_id)
+            db.session.add(post)
+            db.session.commit()
+            response = client.get(f"/post/{post.id}")
+            self.assertEqual(response.status_code, 200)
+
+    def test_edit_post(self):
+        """Test if edit_post updates the selected post"""
+        with app.test_client() as client:
+            post = Post(title="Test Title", content="Test Content",
+                        user_id=self.user_id)
+            db.session.add(post)
+            db.session.commit()
+            response = client.post(f"/edit_post/{post.id}", data={
+                "title": "New Title", "content": "New Content"}, follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(Post.query.first().title, "New Title")
+
+    def test_delete_post(self):
+        """Test if delete_post removes the selected post"""
+        with app.test_client() as client:
+            post = Post(title="Test Title", content="Test Content",
+                        user_id=self.user_id)
+            db.session.add(post)
+            db.session.commit()
+            response = client.post(
+                f"/delete_post/{post.id}", follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(Post.query.count(), 0)
