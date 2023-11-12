2267a33163bffbc10372a4f8ac13103d1bf405f9
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-11-12 00:14:36 -0500

    "began work on adding a post function for users to make posts on their blog"

--- models.py
+++ models.py
@@ -19,3 +19,22 @@ class User(db.Model):
     last_name = db.Column(db.String(50))
     image_url = db.Column(db.String(
         100), nullable=False, default="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg")
+
+    def __repr__(self):
+        u = self
+        return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}>"
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', backref='posts')
